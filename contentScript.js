062009a57e9cc8124e392dba94a32fbc5cfca9f3
nguyenit67 <dehungmanh@gmail.com>
2020-07-06 00:58:12 +0700

    1 file modified

--- contentScript.js
+++ contentScript.js
@@ -1,3 +1,10 @@
+var lbplugin_event_opt = {
+  extension_enable: true,
+  dict_type: 1,
+  dbclk_event: { trigger: "none", enable: true, display: 1 },
+  select_event: { trigger: "none", enable: true, display: 1 }
+};
+
 let callbackFun = code => Function("", `
   ${code}
   lbDictPlugin.init(lbplugin_event_opt);
@@ -5,11 +12,10 @@ let callbackFun = code => Function("", `
 
 let proxyurl = "https://cors-anywhere.herokuapp.com/";
 
-let url = "https://stc-laban.zdn.vn/dictionary/js/plugin/lbdictplugin.min.js?" + Date.now() % 1e4;
+let url =
+  "https://stc-laban.zdn.vn/dictionary/js/plugin/lbdictplugin.min.js?" + Date.now() % 1e4;
 
 fetch(proxyurl + url)
-.then(response =>  response.text())
-.then(callbackFun)
-.catch(error => console.log(error));
-
-
+  .then(response => response.text())
+  .then(callbackFun)
+  .catch(error => console.log(error));
