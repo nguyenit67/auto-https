9b912f641b75cd59325c514058215c5d7e9bf0d8
nguyenit67 <dehungmanh@gmail.com>
2020-07-05 16:17:33 +0700

    1 file modified

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
-chrome.runtime.onInstalled.addListener(() => {
-  
+chrome.runtime.onInstalled.addListener(function() {
+  console.log("Installed! :>");
+  chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
+      chrome.declarativeContent.onPageChanged.addRules([{
+        conditions: [new chrome.declarativeContent.PageStateMatcher({})],
+        actions: [new chrome.declarativeContent.ShowPageAction()]
+      }]);
+  });
 });
