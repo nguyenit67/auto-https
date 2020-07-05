f2add6c99ebf6f83f23ba48b12d0c92664fb01b5
nguyenit67 <dehungmanh@gmail.com>
2020-07-05 18:05:21 +0700

    3 files modified

--- popup.js
+++ popup.js
@@ -1,6 +1,4 @@
 const WIDTH  = 260;
 const HEIGHT = 400;
 
-document.body.style.width = WIDTH;
-
 !function(){var h={s:"https://dict.laban.vn",w:WIDTH,h:HEIGHT,hl:1,th:1};function loadScript(t,e){var n=document.createElement("script");n.type="text/javascript",n.readyState?n.onreadystatechange=function(){("loaded"===n.readyState||"complete"===n.readyState)&&(n.onreadystatechange=null,e())}:n.onload=function(){e()},n.src=t,q=document.getElementById("lbdict_plugin_frame"),q.parentNode.insertBefore(n,q)}setTimeout(function(){loadScript("https://stc-laban.zdn.vn/dictionary/js/plugin/lbdictplugin.frame.min.js",function(){lbDictPluginFrame.init(h)})},1e3); }();
