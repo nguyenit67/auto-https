d9d50ea3189e518adbbc754a78e0f65a93d66b41
nguyenit67 <dehungmanh@gmail.com>
2020-07-05 17:43:37 +0700

    1 file modified

--- background.js
+++ background.js
@@ -4,7 +4,7 @@ chrome.runtime.onInstalled.addListener(function() {
   chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
       chrome.declarativeContent.onPageChanged.addRules([{
         conditions: [
-          // new chrome.declarativeContent.PageStateMatcher({})
+          new chrome.declarativeContent.PageStateMatcher({})
         ],
         actions: [new chrome.declarativeContent.ShowPageAction()]
       }]);
