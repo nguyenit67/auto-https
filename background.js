911e3bd1197acf542d7445ecd79b10d678159833
nguyenit67 <dehungmanh@gmail.com>
2020-07-06 00:31:16 +0700

    2 files modified

--- background.js
+++ background.js
@@ -2,11 +2,3 @@
 chrome.runtime.onInstalled.addListener(function() {
   console.log("Installed! :>");
 });
-chrome.runtime.onMessage.addListener(
-function(message, callback) {
-  if (message === "runContentScript"){
-    chrome.tabs.executeScript({
-      file: 'contentScript.js'
-    });
-  }
-});
