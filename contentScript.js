b8c4714d353a7160725e1945e347c5c028ebe9b4
nguyenit67 <dehungmanh@gmail.com>
2020-07-06 00:54:05 +0700

    1 file modified

--- contentScript.js
+++ contentScript.js
@@ -1,33 +1,15 @@
-// chrome.tabs.executeScript({ code: `
-var lbplugin_event_opt = {
-  extension_enable: true,
-  dict_type: 1,
-  dbclk_event: { trigger: "none", enable: true, display: 1 },
-  select_event: { trigger: "none", enable: true, display: 1 }
-};
-function loadScript(t, e) {
-  var n = document.createElement("script");
-  (n.type = "text/javascript"),
-    n.readyState
-      ? (n.onreadystatechange = function() {
-          ("loaded" === n.readyState || "complete" === n.readyState) &&
-            ((n.onreadystatechange = null), e("From 'OnReadyStateChange' with ❤"));
-        })
-      : (n.onload = function() {
-          e("From 'OnLoad' with ❤");
-        }),
-    (n.src = t),
-    document.getElementsByTagName("head")[0].appendChild(n);
-}
-setTimeout(function() {
-  null == document.getElementById("lbdictex_find_popup") &&
-    loadScript(
-      "https://stc-laban.zdn.vn/dictionary/js/plugin/lbdictplugin.min.js?" +
-        (Date.now() % 1e4),
-      function(msg) {
-        console.log(msg);
-          lbDictPlugin.init(lbplugin_event_opt);
-      }
-    );
-}, 1e3);
-// `});
+let callbackFun = code => Function("", `
+  ${code}
+  lbDictPlugin.init(lbplugin_event_opt);
+`).call();
+
+let proxyurl = "https://cors-anywhere.herokuapp.com/";
+
+let url = "https://stc-laban.zdn.vn/dictionary/js/plugin/lbdictplugin.min.js?" + Date.now() % 1e4;
+
+fetch(proxyurl + url)
+.then(response =>  response.text())
+.then(callbackFun)
+.catch(error => console.log(error));
+
+
